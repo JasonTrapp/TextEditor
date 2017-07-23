158b980e2465d9411730b6c9b94540cbc6da9489
JasonTrapp <jason_t_28@protonmail.ch>
2017-07-23 14:50:05 +1000

    Added comments

--- gui/EditorGUI.java
+++ gui/EditorGUI.java
@@ -16,48 +16,74 @@ public class EditorGUI extends javax.swing.JFrame implements Runnable, ActionLis
 	
 	private int height;
 	
+	/**
+	 * Creates a new Editor GUI with the specified title
+	 * @param title The title of the GUI super class JFrame
+	 */
 	public EditorGUI(String title) {
-		// TODO Auto-generated constructor stub
+		super(title);
 		this.setWidth(DEFAULT_WIDTH);
 		this.setHeight(DEFAULT_HEIGHT);
 	}
 	
+	/**
+	 * Gets the current height of the GUI
+	 * @return The height of the GUI
+	 */
 	public int getHeight(){
 		return height;
 	}
 	
+	/**
+	 * Sets the new height of the JFrame.
+	 * @param height The height in which to make the JFrame
+	 */
 	public void setHeight(int height){
 		this.height = height;
 	}
 	
+	/**
+	 * Gets the current width of the GUI
+	 * @return The width of the GUI
+	 */
 	public int getWidth(){
 		return width;
 	}
 	
+	/**
+	 * Sets the new width of the JFrame.
+	 * @param height The width in which to make the JFrame
+	 */
 	public void setWidth(int width){
 		this.width = width;
 	}
 
+	/**
+	 * Handles the actions performed by the user such as button clicking
+	 * @param e called when action has occurred
+	 */
 	@Override
-	public void actionPerformed(ActionEvent arg0) {
+	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
 
+	/**
+	 * 
+	 */
 	@Override
 	public void run() {
 		createGUI();
 	}
 	
+	/**
+	 * Creates the GUI, sets default variables and components for GUI and interaction.
+	 */
 	public void createGUI(){
 		setSize(this.getWidth(), this.getHeight());
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		
-		
 		this.setVisible(true);
-		
 	}
-
 }
